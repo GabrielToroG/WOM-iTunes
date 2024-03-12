aaae492e872b5ca86cf5d2ecf454857c9bd2fdd1
Gabriel Toro <gtorowork@gmail.com>
2024-03-12 14:24:54 -0300

    set GetItunesListUseCase

--- WOM-iTunes/AppModule/Home/Domain/UseCase/GetItunesListUseCase.swift
+++ WOM-iTunes/AppModule/Home/Domain/UseCase/GetItunesListUseCase.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+final class GetItunesListUseCase: ClosureUseCase {
+    typealias RequestType = iTunesListQueryParams
+    typealias ResultType = UiiTunesResponse
+    
+    let repository: HomeDataSource
+    let mapper: HomeDomainMapper
+    
+    init(repository: HomeDataSource, mapper: HomeDomainMapper) {
+        self.repository = repository
+        self.mapper = mapper
+    }
+    
+    func execute(
+        _ parameters: RequestType,
+        _ onCompletion: @escaping (ApiResult<ResultType>) -> Void
+    ) {
+        repository.getItunesList(queryParams: parameters) { [weak self] in
+            guard let self = self else { return }
+            onCompletion($0.map(self.mapper.domainToPresentation(_:)))
+        }
+    }
+}
